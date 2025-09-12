da0cd2cae88b5f0d71bacaf470778e4779e82568
sag030601 <121371237+sag030601@users.noreply.github.com>
2025-09-12 15:54:22 +0530

    Update prisma.ts

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,6 +1,6 @@
 // import { PrismaClient } from '../generated/prisma';
 import { PrismaClient } from '@prisma/client';
-
+// import { PrismaClient } from '../generated/prisma';
 
 const prisma = new PrismaClient();
 
